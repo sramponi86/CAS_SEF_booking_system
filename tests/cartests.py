98fdaae645724b9edb20d53bd1bc1d166c579e62
Thorsten Sittig <tsittig@ethz.ch>
2024-05-23 18:14:17 +0200

    fixed car.getColor test

--- tests/cartests.py
+++ tests/cartests.py
@@ -12,9 +12,9 @@ class CarTests(unittest.TestCase):
     self.assertEqual(car.id, 1, 'incorrect id after construction')
     self.assertEqual(car.model, 'Opel Kadett', 'incorrect model after construction')
 
-  def test_getColor(self)
+  def test_getColor(self):
     car = Car(1, 'Opel Kadett', 'orange', "B") 
-    self.assertEqual(car.getColor(), 'orange', 'incorrect color retrieved')
+    self.assertEqual(car.getColor(), 'orange', 'Incorrect color retrieved')
 
 class CarsTests(unittest.TestCase):
   def setUp(self):
